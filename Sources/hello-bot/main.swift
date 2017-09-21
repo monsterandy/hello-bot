ec329331267f08c4f499178ca340104b2f42a466
monsterandy <294399850@qq.com>
2017-09-21 20:33:32 +0800

    Router Added.

--- Sources/hello-bot/main.swift
+++ Sources/hello-bot/main.swift
@@ -1,13 +1,31 @@
 import Foundation
 import TelegramBot
 
-print("Hello, world!")
 
 let token = readToken(from: "HELLO_BOT_TOKEN")
-print(token)
+//print(token)
 let bot = TelegramBot(token: token)
 
+let router = Router(bot: bot)
+
+router["help", .slashRequired] = { context in
+    guard let from = context.message?.from else { return false }
+    let helpText = "Usage: /greet"
+    context.respondPrivatelyAsync(helpText, groupText: "\(from.first_name), please find usage instructions in a personal message.")
+    return true
+}
+
+router["greet", .slashRequired] = { context in
+    guard let from = context.message?.from else { return false }
+    context.respondAsync("Hello, \(from.first_name)!")
+    return true
+}
+
+print("Ready to accept commands")
+
 while let update = bot.nextUpdateSync() {
+    print("--- update: \(update.debugDescription)")
+    try router.process(update: update)
     if let message = update.message, let from = message.from, let text = message.text {
         bot.sendMessageAsync(chat_id: from.id,
                              text: "Hi \(from.first_name)! You said: \(text).\n")
@@ -15,5 +33,7 @@ while let update = bot.nextUpdateSync() {
 }
 
 let defaultError = "Unknow Error!"
-fatalError("Server stopped due to error: \(String(describing: bot.lastError))")
+fatalError("Server stopped due to error: \(bot.lastError.unwrapOptional)")
+
+
 
